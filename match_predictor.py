c7ced79264ad47cf48ee2f1876eafefd080541b2
thebhatman <manjunathbhat9920@gmail.com>
2018-10-30 23:07:52 +0530

    Random Forest Clf giving 97% acc/season

--- match_predictor.py
+++ match_predictor.py
@@ -7,6 +7,7 @@ import xgboost as xgb
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import make_scorer
 from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
 #from sklearn.cross_validation import test_train_split
 #from sklearn.preprocessing import scale
 
@@ -35,29 +36,32 @@ for i in range(len(ipdf)):
 	ipdf['DiffLP'][i] = ipdf['DiffLP'][i] /5
 
 y_all = input_data.loc[:,'FTR']
-x_train = ipdf.loc[0:6359,:]
-y_train = y_all[0:6360]
-x_test = ipdf.loc[6360:6460,:]
-y_test = y_all[6360:6460]
+x_train = ipdf.loc[0:6079,:]
+y_train = y_all[0:6080]
+x_test = ipdf.loc[6080:6460,:]
+y_test = y_all[6080:6460]
 
-classifier = svm.SVC(gamma = 1, kernel = 'rbf', C = 750)
-classifier.fit(x_train,y_train)
-Y_out = classifier.predict(x_test)
+# classifier = svm.SVC(gamma = 1, kernel = 'rbf', C = 500)
+# classifier.fit(x_train,y_train)
+# Y_out = classifier.predict(x_test)
 #print Y_out
 
-clf = xgb.XGBClassifier(seed = 82)
-parameters = {'learning_rate':[0.1], 'n_estimators':[40],'max_depth':[3],'min_child_weight':[3], 'gamma':[0.4],'subsample':[0.8], 'colsample_bytree':[0.8], 'scale_pos_weight' : [1], 'reg_alpha' : [1e-5]}
-f1_scorer = make_scorer(f1_score,pos_label='H', average = 'weighted') 
+# clf = xgb.XGBClassifier(seed = 82)
+# parameters = {'learning_rate':[0.1], 'n_estimators':[40],'max_depth':[3],'min_child_weight':[3], 'gamma':[0.4],'subsample':[0.8], 'colsample_bytree':[0.8], 'scale_pos_weight' : [1], 'reg_alpha' : [1e-5]}
+# f1_scorer = make_scorer(f1_score,pos_label='H', average = 'weighted') 
 
 # grid_obj = GridSearchCV(clf, scoring = f1_scorer, param_grid = parameters, cv = 5)
 # grid_obj = grid_obj.fit(x_train, y_train)
 # clf = grid_obj.best_estimator_
 # clf.fit(x_train, y_train)
 # Y_out = clf.predict(x_test)
-
+forest_clf = RandomForestClassifier(n_estimators = 100, max_depth = 20, random_state = 0)
+forest_clf.fit(x_train, y_train)
+Y_out = forest_clf.predict(x_test)
 correct_pred = 0
 for i in range(len(Y_out)):
-	if Y_out[i] == y_test[i+6360]:
+	if Y_out[i] == y_test[i+6080]:
 		correct_pred+=1
 
-print(float(correct_pred))
+print(float(correct_pred/3.8))
+
